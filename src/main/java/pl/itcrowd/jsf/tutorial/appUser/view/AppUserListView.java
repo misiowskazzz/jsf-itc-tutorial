519d99f6ffe5b619bf094b788e4b527dd3907892
misiowskazzz <misiowskazzz@gmail.com>
2013-02-19 10:47:04 +0100

    simple use of param

--- src/main/java/pl/itcrowd/jsf/tutorial/appUser/view/AppUserListView.java
+++ src/main/java/pl/itcrowd/jsf/tutorial/appUser/view/AppUserListView.java
@@ -77,4 +77,14 @@ public class AppUserListView {
         }
         return msg;
     }
+
+    private int counter=1;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
 }
